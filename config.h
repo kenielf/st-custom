a90c76e113621fb571bc12ec7977644706994f41
kenielf <chrystian.keni.github@gmail.com>
2024-01-22 17:05:37 -0300

    Applied scrollback patches.

--- config.h
+++ config.h
@@ -166,6 +166,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1} },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -178,19 +180,21 @@ static MouseShortcut mshortcuts[] = {
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
-	/* mask                        keysym          function        argument */
-	{ XK_ANY_MOD,                  XK_Break,       sendbreak,      {.i =  0} },
-	{ ControlMask,                 XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,                   XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,                  XK_Print,       printsel,       {.i =  0} },
-	{ ShiftMask | ControlMask,     XK_plus,        zoom,           {.f = +4} },
-	{ ShiftMask | ControlMask,     XK_underscore,  zoom,           {.f = -4} },
-	{ ShiftMask | ControlMask,     XK_BackSpace,   zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	/* mask                     keysym          function        argument */
+	{ XK_ANY_MOD,               XK_Break,       sendbreak,      {.i =  0} },
+	{ ControlMask,              XK_Print,       toggleprinter,  {.i =  0} },
+	{ ShiftMask,                XK_Print,       printscreen,    {.i =  0} },
+	{ XK_ANY_MOD,               XK_Print,       printsel,       {.i =  0} },
+	{ ShiftMask | ControlMask,  XK_plus,        zoom,           {.f = +4} },
+	{ ShiftMask | ControlMask,  XK_underscore,  zoom,           {.f = -4} },
+	{ ShiftMask | ControlMask,  XK_BackSpace,   zoomreset,      {.f =  0} },
+	{ TERMMOD,                  XK_C,           clipcopy,       {.i =  0} },
+	{ TERMMOD,                  XK_V,           clippaste,      {.i =  0} },
+	{ TERMMOD,                  XK_Y,           selpaste,       {.i =  0} },
+	{ ShiftMask,                XK_Insert,      selpaste,       {.i =  0} },
+	{ TERMMOD,                  XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,                XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,                XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
